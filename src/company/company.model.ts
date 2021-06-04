0d2995ac2467c873a90fa11254b8cb059b8d2eca
Karl Agunday <kaagunday@gmail.com>
2021-06-04 16:24:25 +0800

    remove unusued variable

--- src/company/company.model.ts
+++ src/company/company.model.ts
@@ -5,7 +5,7 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 // using assertions (!) on fields to ignore issues with having no constructor
 // see https://github.com/nestjs/nest/issues/4178#issuecomment-613197634
 export class Company {
-  @Field((type) => ID)
+  @Field(() => ID)
   id!: number;
 
   @Field()
