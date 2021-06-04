4ace4db6ab9843dc85e9bf75f06f8399ee6e9950
Karl Agunday <kaagunday@gmail.com>
2021-06-04 15:44:26 +0800

    retrieve all companies

---
+++ src/company/company.model.ts
@@ -0,0 +1,13 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+
+// using assertions (!) on fields to ignore issues with having no constructor
+// see https://github.com/nestjs/nest/issues/4178#issuecomment-613197634
+export class Company {
+  @Field((type) => ID)
+  id!: number;
+
+  @Field()
+  title!: string;
+}
